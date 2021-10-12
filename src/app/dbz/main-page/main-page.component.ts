f590625b51e53047f13d3a494e5a97cccedc4499
Jakson Casas <jaksoncasas@gmail.com>
2021-10-12 08:34:18 -0500

    Add item over component and show list with ngFor

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -11,16 +11,34 @@ interface Personaje{
 })
 export class MainPageComponent {
 
-  
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre:'Vegeta',
+      poder:7500
+    }
+  ];
+
   nuevo:Personaje = {
-    nombre: 'Trucks',
-    poder: 14000
+    nombre: '',
+    poder: 0
   }
   /*cambiarNombre(event:any){
     console.log(event.target.value);
   }*/
   agregar(){
-    console.log(this.nuevo);
+    if (this.nuevo.nombre.trim().length === 0){
+      return;
+    }
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+
   }
 
 }
