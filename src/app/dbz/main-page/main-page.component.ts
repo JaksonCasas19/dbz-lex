f5f5f5072ccaa0940a8e6a760e8548890212bc49
Jakson Casas <jaksoncasas@gmail.com>
2021-10-12 23:27:38 -0500

    Add new component sound

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -19,7 +19,22 @@ export class MainPageComponent {
     }
   ];
 
- 
+
+  nuevo: Personaje = {
+    nombre:'Maestro Roshi',
+    poder:1000
+  }
+
+  agregarNuevoPersonaje(argumento:Personaje){
+    this.personajes.push(argumento);
+    this.play()
+  }
+
+  play(){
+    var audio = new Audio('/assets/new-message-alert.mp3');
+    audio.play();
+}
+
   /*cambiarNombre(event:any){
     console.log(event.target.value);
   }*/
